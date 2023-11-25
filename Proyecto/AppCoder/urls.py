8306768dd0d48bd5ff2294f759dfbd6ce918fd8a
NicolasPalacios <palaciosnicolas94@gmail.com>
2023-11-25 12:27:45 -0300

    agreguue template

---
+++ Proyecto/AppCoder/urls.py
@@ -0,0 +1,10 @@
+
+
+from django.urls import path
+from django.http import HttpResponse
+from AppCoder.views import view_cursos, view_inicio
+
+urlpatterns = [
+    path('inicio/', view_inicio),
+    path('cursos/', view_cursos)
+]
--- Proyecto/AppCoder/views.py
+++ Proyecto/AppCoder/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def view_inicio(xx):
+    return HttpResponse( "BIENVENIDOS")
+
+def view_cursos(xx):
+    return render(xx, "AppCoder/padre.html")
+
+
